e18caab94d98878043dd2a2ee0a18f732de58192
BianLong0618 <bianlong0618@qq.com>
2021-12-09 23:56:26 +0800

    Update the code for the two sum (Leetcode01).

--- main.go
+++ main.go
@@ -4,4 +4,25 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, Leetcode!")
+
+	nums01 := []int{2, 7, 11, 15}
+	target := 9
+
+	fmt.Println(twoSum(nums01, target))
+}
+
+//  Leetcode 1. Two Sum(Easy), #1
+func twoSum(nums []int, target int) (ans []int) {
+	hashtable := make(map[int]int)
+	for i := 0; i < len(nums); i++ {
+		key := target - nums[i]
+		if _, ok := hashtable[key]; ok {
+			ans = append(ans, hashtable[key])
+			ans = append(ans, i)
+			break
+		} else {
+			hashtable[nums[i]] = i
+		}
+	}
+	return
 }
