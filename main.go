dd499912ee9fde1526b0c940103eaa2bca96e4b6
BianLong0618 <bianlong0618@qq.com>
2021-12-10 23:59:06 +0800

    Update the code for the maxArea (Leetcode11).

--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Hello, Leetcode!")
 
 	nums01 := []int{2, 7, 11, 15}
 	target := 9
-
+	//  Leetcode 01
 	fmt.Println(twoSum(nums01, target))
+	//  Leetcode 11
+	fmt.Println(maxArea([]int{1, 8, 6, 2, 5, 4, 8, 3, 7}))
 }
 
 //  Leetcode 1. Two Sum(Easy), #1
@@ -25,4 +29,36 @@ func twoSum(nums []int, target int) (ans []int) {
 		}
 	}
 	return
+}
+
+func maxArea(height []int) int {
+
+	i, j, area := 0, len(height)-1, 0
+
+	for i < j {
+		area = max(area, (j-i)*min(height[i], height[j]))
+		if height[i] < height[j] {
+			i++
+		} else {
+			j--
+		}
+	}
+	return area
+}
+
+func min(a int, b int) int {
+	if a < b {
+		return a
+	} else {
+		return b
+	}
+
+}
+
+func max(a int, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
 }
